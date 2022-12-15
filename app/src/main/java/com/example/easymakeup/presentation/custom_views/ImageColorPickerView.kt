d08392a1f97d739d86310b6438c72d29a1d338cf
Misa <milosursulovic2696@gmail.com>
2022-12-15 22:55:31 +0100

    Remove custom view classes

--- app/src/main/java/com/example/easymakeup/presentation/custom_views/ImageColorPickerView.kt
+++
@@ -1,8 +0,0 @@
-package com.example.easymakeup.presentation.custom_views
-
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-
-class ImageColorPickerView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-}
