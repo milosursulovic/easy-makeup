8909e4a94a1b159a7bd73858b7bebca3fb86a35c
Misa <milosursulovic2696@gmail.com>
2022-12-16 19:41:15 +0100

    Add two custom views

---
+++ app/src/main/java/com/example/easymakeup/presentation/custom_views/ImageColorPickView.kt
@@ -0,0 +1,8 @@
+package com.example.easymakeup.presentation.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ImageColorPickView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
---
+++ app/src/main/java/com/example/easymakeup/presentation/custom_views/StepView.kt
@@ -0,0 +1,8 @@
+package com.example.easymakeup.presentation.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class StepView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+}
